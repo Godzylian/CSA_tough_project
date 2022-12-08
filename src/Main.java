a792f73cac21822a0b144e9812a77e499f8c89d2
Joe Lewis <joelewis@gmail.com>
2022-12-07 21:43:12 -0800

    Update Main.java
    final updates

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         // Assign 10 student names to an array:
@@ -53,8 +55,14 @@ public class Main {
 
         // Create array `tied_names` based on `dupe_count` size, to hold the top items.
         // Then, add the top winning names to the `tied_names` array.
+        //
+        // If there's more than 1 winner:
         if (dupe_count > 1) {
+            // Create an array to hold the list of tied names.
             String[] tied_names = new String[dupe_count];
+            // Create array to hold the index number of each tied name.
+            int[] names_index = new int[dupe_count];
+            // Incrementer for the loop.
             int tied_names_index = 0;
 
             // Print initial text:
@@ -62,23 +70,41 @@ public class Main {
 
             // Loop: If the `asterisk_count[i]` is equal to `biggest`, add name to `tied_names`
             // Manually increment a counter every time something is added to `tied_names`, so
-            // you know what node of `tied_names` you're adding to.
-            if (dupe_count > 1) {
-                for (int i = 0; i < asterisk_count.length; i++) {
-                    if (asterisk_count[i] == biggest) {
-                        tied_names[tied_names_index] = names[i];
-                        System.out.print(names[i]);
-                        if (tied_names_index < dupe_count - 1) {
-                            System.out.print(" and ");
-                        }
-                        tied_names_index++;
+            // you know what node of `tied_names` you're adding to. Print results.
+            for (int i = 0; i < asterisk_count.length; i++) {
+                if (asterisk_count[i] == biggest) {
+                    tied_names[tied_names_index] = names[i];
+                    names_index[tied_names_index] = i;
+                    System.out.print(names[i]);
+                    if (tied_names_index < dupe_count - 1) {
+                        System.out.print(" and ");
                     }
+                    tied_names_index++;
                 }
-                System.out.println();
-                System.out.println("Student selected: foo, foo (add code for multiple winners)");
-            } else {
-                System.out.println("Student selected: (add code for one winner");
             }
+            System.out.println();
+
+            // Tiebreaker routine
+            // Pick a random winner from `tied_names`
+            Random rand = new Random();
+            int random_winner = rand.nextInt(tied_names.length);
+
+            // Print winner.
+            System.out.println("Student selected: " + (names_index[random_winner] + 1) + ", " +
+                names[names_index[random_winner]]);
+        } else {
+            // if there's only one winner, we don't have as much to process.
+            // Create a string and an int to hold the name of the winner and the winner's index from
+            // the `name` array
+            String winner = "nerf";
+            int name_index = -1;
+            for (int i = 0; i < asterisk_count.length; i++) {
+                if (asterisk_count[i] == biggest) {
+                    winner = names[i];
+                    name_index = i;
+                }
+            }
+            System.out.println("Student selected: " + (name_index + 1) + ", " + winner);
         }
     }
 }
